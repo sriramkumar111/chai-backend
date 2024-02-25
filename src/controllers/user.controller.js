3b16c99d9deebdfb19ba1ac056668b8e5b17e340
SRI RAM KUMAR <sri.ramkumar.21@aot.edu.in>
2024-02-25 19:43:18 +0530

    getUserChannelProfile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -263,7 +263,7 @@ const refreshAccessToken = asyncHandler(async (req, res) =>{
    }
 
 })
-//change currre3nt password
+//change currrent password
 
 const changeCurrentPassword = asyncHandler (async (req, res) => {
 
@@ -290,7 +290,13 @@ const changeCurrentPassword = asyncHandler (async (req, res) => {
 const getCurrentUser = asyncHandler (async(req, res) => {
     return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully")
+    .json(new ApiResponse (
+        200,
+        req.user,
+        "user fetched successfully"
+    )
+    )
+       
 })
 
 const updateAccountDetails = asyncHandler(async( req, res) =>{
@@ -303,7 +309,7 @@ const updateAccountDetails = asyncHandler(async( req, res) =>{
 
     //sending the information to update fullName and email
 
-    const user = User.findByIdAndUpdate(
+    const user =await  User.findByIdAndUpdate(
         req.user?._id,
         {
             $set: {
@@ -330,6 +336,8 @@ const updateUserAvatar = asyncHandler(async(req, res ) => {
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is missing")
     }
+   //TODO : delete old image -assignment
+
 
     //if we got files then upload on cloudinary
     const avatar = await uploadOnCloudinary(avatarLocalPath)
@@ -389,6 +397,91 @@ const updateUserCoverImage = asyncHandler(async(req, res ) => {
     )
 })
 
+//GETTING USER CHANNER PROFILE
+const getUserChannelProfile  = asyncHandler (async(req, res ) => {
+    const {username } = req.params
+
+    if(!username ?.trim()){
+        throw new ApiError(400, "username is missing")
+    }
+
+    const channel = await User.aggregate([
+        //stage 1 match the dcoument
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+        //find the user subscriber (stage 2)
+        {
+            $lookup : {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+
+        {
+            $lookup : {
+                from : "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo"
+
+
+            }
+        },
+        // add more informations to the current document
+        {
+            $addFields: {
+                subscribersCount : {
+                    $size: "$subscribers"
+                },
+                channelsSubscribedToCount: {
+                    $size: "$subscribedTo"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]}, //yahan field se value le rahe hai isliey $ lagaye hai
+                        then: true,
+                        else: false
+                    }
+                }
+            }
+
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+
+
+
+            }
+        }
+
+    ])
+    //agar channel me kuchh aaya hi nahi to
+
+    if(!channel?.length){
+        throw new ApiError(404, "channel does not exists")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, channel[0], "User channel fetched successfulley")
+    )
+
+})
+
 export {
     registerUser,
     loginUser,
@@ -398,5 +491,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile
 }
